3f3a832a4e230d40489c4cd4853e1613a8faabfe
m3ru <merugopalan2007@gmail.com>
2026-01-05 21:10:19 -0500

    deploy script for local side

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Auto-deploy wiki to Cloudflare Pages
+
+set -e
+
+cd "$(dirname "$0")"
+
+# Export org-roam notes to markdown
+cd ~/org-roam
+emacs --batch --load ~/.emacs.d/init.el wiki.org --eval '(org-hugo-export-wim-to-md :all-subtrees)' 2>&1
+
+# Commit and push changes
+cd ~/wiki
+git add content/
+git diff-index --quiet HEAD || git commit -m "Update wiki content auto-deployed from org-roam"
+git push origin master
+
