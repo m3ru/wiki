09eb9802a211893d7f443b5dc2ccee77a08fb54a
m3ru <merugopalan2007@gmail.com>
2026-01-05 21:42:23 -0500

    change base url

--- public/en.search-data.min.1a7d715dfad399506fe3677286b5b55d9d7e3622d83bd864aef0a176537f134a.js
+++ public/en.search-data.min.1a7d715dfad399506fe3677286b5b55d9d7e3622d83bd864aef0a176537f134a.js
@@ -1,4 +1,4 @@
-"use strict";(function(){const t={};t.doc={id:"id",field:["title","content"],store:["title","href"]};const e=FlexSearch.create("balance",t);window.bookSearchIndex=e,e.add({id:0,href:"/posts/anushka/",title:"Anushka",content:`Anushka Poddar is my current girlfriend who(m?) I love.
+"use strict";(function(){const t={};t.doc={id:"id",field:["title","content"],store:["title","href"]};const e=FlexSearch.create("balance",t);window.bookSearchIndex=e,e.add({id:0,href:"/posts/anushka/",title:"Girlfriend",content:`Anushka Poddar is my current girlfriend who(m?) I love.
 `}),e.add({id:1,href:"/posts/hiremagalur/",title:"Hiremagalur",content:`Does this even work? I&rsquo;m not convinced that writing in these notes actually makes sense for people to construct their personal wiki with.
 To understand the name Hiremagalur: (hire) comes from &ldquo;Hiriya&rdquo; or big. (magal) or daughter. (ur) comes from ooru or town. (hiremagalur) or town [of the] eldest daughter
 `}),e.add({id:2,href:"/wiki/hiremagalur/",title:"Hiremagalur",content:`Does this even work? I&rsquo;m not convinced that writing in these notes actually makes sense for people to construct their personal wiki with.
