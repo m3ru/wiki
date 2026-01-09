7209a74e23d799e0996edee0bc6864c5fbba17b0
m3ru <merugopalan2007@gmail.com>
2026-01-09 15:00:28 -0500

    change to sync script

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ set -e
 cd "$(dirname "$0")"
 
 # Export org-roam notes to markdown
-cd ~/org-roam
+cd ~/sync/org-roam
 emacs --batch --load ~/.emacs.d/init.el wiki.org --eval '(org-hugo-export-wim-to-md :all-subtrees)' 2>&1
 
 # Commit and push changes
-cd ~/wiki
+cd ~/sync/wiki
 git add content/
 git diff-index --quiet HEAD || git commit -m "Update wiki content auto-deployed from org-roam"
 git push origin master
